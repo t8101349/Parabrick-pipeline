dbac975daaf89d02eff7587f4b4adc18d43a4a35
t8101349 <30904854+t8101349@users.noreply.github.com>
2025-11-25 10:07:30 +0800

    Update parabrick.sh

--- parabrick.sh
+++ parabrick.sh
@@ -1,33 +1,62 @@
-wget -c ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/hg38/dbsnp_146.hg38.vcf.gz
-wget -c ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/hg38/dbsnp_146.hg38.vcf.gz.tbi
+# === 參數解析 ===
+re='^(--help|-h)$'
+if [[ $1 =~ $re ]]; then
+    Help
+else
+    while [ "$#" -gt 0 ]; do
+        case "$1" in
+            -r|--ref) ref="$2"; shift 2;;
+            -s|--sample) sample="$2"; shift 2;;
+            -k|--knownSites) knownSites="$2"; shift 2;;
+            -f|--filter) out="$2"; shift 2;;
+            *) echo "unknown option: $1" >&2; exit 1;;
+        esac
+    done
 
-wget https://storage.googleapis.com/genomics-public-data/resources/broad/hg38/v0/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz
-wget https://storage.googleapis.com/genomics-public-data/resources/broad/hg38/v0/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz.tbi
+    # === 檢查必要參數 ===
+    if [[ -z "$sample" ]]; then
+        echo "❌ 必要參數缺失，請確認 --sample 是否有指定。" >&2
+        exit 1
+    fi
+    if [[ -z "$ref" ]]; then
+        ref="Homo_sapiens_assembly38.fasta"
+    fi
+    if [[ -z "$filter" ]]; then
+        filter="3.0103"
+    fi
 
-tar xvf parabricks_sample.tar.gz
-parabricks_sample/ parabricks_sample/Data/ parabricks_sample/Data/markdup_input.bam parabricks_sample/Data/sample_2.fq.gz parabricks_sample/Data/sample_1.fq.gz parabricks_sample/Data/single_ended.bam parabricks_sample/Ref/ parabricks_sample/Ref/Homo_sapiens_assembly38.fasta parabricks_sample/Ref/Homo_sapiens_assembly38.fasta.pac parabricks_sample/Ref/Homo_sapiens_assembly38.fasta.ann parabricks_sample/Ref/Homo_sapiens_assembly38.known_indels.vcf.gz.tbi parabricks_sample/Ref/Homo_sapiens_assembly38.fasta.amb parabricks_sample/Ref/Homo_sapiens_assembly38.dict parabricks_sample/Ref/Homo_sapiens_assembly38.fasta.fai parabricks_sample/Ref/Homo_sapiens_assembly38.known_indels.vcf.gz parabricks_sample/Ref/Homo_sapiens_assembly38.fasta.bwt parabricks_sample/Ref/Homo_sapiens_assembly38.fasta.sa
+
+
+#wget -c ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/hg38/dbsnp_146.hg38.vcf.gz
+#wget -c ftp://gsapubftp-anonymous@ftp.broadinstitute.org/bundle/hg38/dbsnp_146.hg38.vcf.gz.tbi
+
+#wget https://storage.googleapis.com/genomics-public-data/resources/broad/hg38/v0/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz
+#wget https://storage.googleapis.com/genomics-public-data/resources/broad/hg38/v0/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz.tbi
+
+#tar xvf parabricks_sample.tar.gz
+#parabricks_sample/ parabricks_sample/Data/ parabricks_sample/Data/markdup_input.bam parabricks_sample/Data/sample_2.fq.gz parabricks_sample/Data/sample_1.fq.gz #parabricks_sample/Data/single_ended.bam parabricks_sample/Ref/ parabricks_sample/Ref/Homo_sapiens_assembly38.fasta parabricks_sample/Ref/Homo_sapiens_assembly38.fasta.pac #parabricks_sample/Ref/Homo_sapiens_assembly38.fasta.ann parabricks_sample/Ref/Homo_sapiens_assembly38.known_indels.vcf.gz.tbi #parabricks_sample/Ref/Homo_sapiens_assembly38.fasta.amb parabricks_sample/Ref/Homo_sapiens_assembly38.dict parabricks_sample/Ref/Homo_sapiens_assembly38.fasta.fai #parabricks_sample/Ref/Homo_sapiens_assembly38.known_indels.vcf.gz parabricks_sample/Ref/Homo_sapiens_assembly38.fasta.bwt #parabricks_sample/Ref/Homo_sapiens_assembly38.fasta.sa
 
 
 # 合併 L1~L8 的 R1 / R2
-#cat /mnt/NovaSeqX/GalateaBio/plate1/fastq/00015988_2025-09-12_L*_ds.*/*R1_001.fastq.gz > /home/weber/tmp/00015988_2025-09-12_R1_all.fastq.gz
-#cat /mnt/NovaSeqX/GalateaBio/plate1/fastq/00015988_2025-09-12_L*_ds.*/*R2_001.fastq.gz > /home/weber/tmp/00015988_2025-09-12_R2_all.fastq.gz
+#cat /mnt/NovaSeqX/GalateaBio/plate1/fastq/${sample}_L*_ds.*/*R1_001.fastq.gz > /home/weber/tmp/${sample}_R1_all.fastq.gz
+#cat /mnt/NovaSeqX/GalateaBio/plate1/fastq/${sample}_L*_ds.*/*R2_001.fastq.gz > /home/weber/tmp/${sample}_R2_all.fastq.gz
 
 # 合併 R1
 (
 for LANE in {1..8}; do
-    DIR=$(ls -d /mnt/NovaSeqX/GalateaBio/plate1/fastq/00015988_2025-09-12_L${LANE}_ds.*)
+    DIR=$(ls -d /mnt/NovaSeqX/GalateaBio/plate1/fastq/${sample}_L${LANE}_ds.*)
     ls ${DIR}/*R1_001.fastq.gz | sort
 done
-) | xargs cat > /home/weber/tmp/00015988_2025-09-12_R1_all.fastq.gz
+) | xargs cat > /home/weber/tmp/${sample}_R1_all.fastq.gz
 # 合併 R2
 (
 for LANE in {1..8}; do
-    DIR=$(ls -d /mnt/NovaSeqX/GalateaBio/plate1/fastq/00015988_2025-09-12_L${LANE}_ds.*)
+    DIR=$(ls -d /mnt/NovaSeqX/GalateaBio/plate1/fastq/${sample}_L${LANE}_ds.*)
     ls ${DIR}/*R2_001.fastq.gz | sort
 done
-) | xargs cat > /home/weber/tmp/00015988_2025-09-12_R2_all.fastq.gz
+) | xargs cat > /home/weber/tmp/${sample}_R2_all.fastq.gz
 
-
+mkdir -p /home/weber/QC/${sample}
 # 前qc
 docker run \
   --rm \
@@ -35,22 +64,21 @@ docker run \
   -v /home:/home \
   biocontainers/fastqc:v0.11.9_cv8 \
   fastqc \
-    /home/weber/tmp/00015988_2025-09-12_R1_all.fastq.gz \
-    /home/weber/tmp/00015988_2025-09-12_R2_all.fastq.gz \
-    -o /home/weber/QC/test_sample/
+    /home/weber/tmp/${sample}_R1_all.fastq.gz \
+    /home/weber/tmp/${sample}_R2_all.fastq.gz \
+    -o /home/weber/QC/${sample}/
 
 
 # trim
-sample=00015988_2025-09-12
 out_dir=/home/weber/fastp_trim/${sample}
 mkdir -p $out_dir
 
 docker run --rm -v /home:/home quay.io/biocontainers/fastp:0.23.2--h79da9fb_0 \
   fastp \
-    -i /home/weber/tmp/00015988_2025-09-12_R1_all.fastq.gz \
-    -I /home/weber/tmp/00015988_2025-09-12_R2_all.fastq.gz \
-    -o ${out_dir}/00015988_2025-09-12_R1_trim.fastq.gz \
-    -O ${out_dir}/00015988_2025-09-12_R2_trim.fastq.gz \
+    -i /home/weber/tmp/${sample}_R1_all.fastq.gz \
+    -I /home/weber/tmp/${sample}_R2_all.fastq.gz \
+    -o ${out_dir}/${sample}_R1_trim.fastq.gz \
+    -O ${out_dir}/${sample}_R2_trim.fastq.gz \
     -q 30 -u 50 -l 40 -w 16 -t 4 -T 4 \
     --trim_poly_g \
     -h ${out_dir}/R1_fastp.html \
@@ -63,27 +91,12 @@ docker run \
   -v /home:/home \
   biocontainers/fastqc:v0.11.9_cv8 \
   fastqc \
-    /home/weber/fastp_trim/00015988_2025-09-12/00015988_2025-09-12_R1_trim.fastq.gz \
-    /home/weber/fastp_trim/00015988_2025-09-12/00015988_2025-09-12_R2_trim.fastq.gz \
-    -o /home/weber/QC/test_sample/
+    /home/weber/fastp_trim/${sample}/${sample}_R1_trim.fastq.gz \
+    /home/weber/fastp_trim/${sample}/${sample}_R2_trim.fastq.gz \
+    -o /home/weber/QC/${sample}/
 
-"""
-# fq2bam
-docker run \
-  --gpus all \
-  --rm \
-  --volume /home:/home \
-  --volume /mnt:/mnt \
-  nvcr.io/nvidia/clara/clara-parabricks:4.6.0-1 \
-pbrun fq2bam \
-    --ref /home/weber/parabricks_sample/Ref/Homo_sapiens_assembly38.fasta \
-    --in-fq /home/weber/fastp_trim/00015988_2025-09-12/00015988_2025-09-12_R1_trim.fastq.gz \
-            /home/weber/fastp_trim/00015988_2025-09-12/00015988_2025-09-12_R2_trim.fastq.gz \
-    --read-group-id-prefix sample1 --read-group-sm sample1 --read-group-lb lib1 --read-group-pl ILLUMINA \
-    --out-bam /home/weber/outputdir/fq2bam_output.bam \
-    --out-duplicate-metrics /home/weber/outputdir/sample1.dup_metrics.txt 
-"""
 
+mkdir -p /home/weber/outputdir/${sample}
 # fq2bam 含oom應對方案
 docker run \
   --gpus '"device=1,2,3,4"' \
@@ -92,12 +105,12 @@ docker run \
   --volume /mnt:/mnt \
   nvcr.io/nvidia/clara/clara-parabricks:4.6.0-1 \
 pbrun fq2bam \
-    --ref /home/weber/parabricks_sample/Ref/Homo_sapiens_assembly38.fasta \
-    --in-fq /home/weber/fastp_trim/00015988_2025-09-12/00015988_2025-09-12_R1_trim.fastq.gz \
-            /home/weber/fastp_trim/00015988_2025-09-12/00015988_2025-09-12_R2_trim.fastq.gz \
-    --read-group-id-prefix sample1 --read-group-sm sample1 --read-group-lb lib1 --read-group-pl ILLUMINA \
-    --out-bam /home/weber/outputdir/fq2bam_output.bam \
-    --out-duplicate-metrics /home/weber/outputdir/sample1.dup_metrics.txt \
+    --ref /home/weber/parabricks_sample/Ref/${ref} \
+    --in-fq /home/weber/fastp_trim/${sample}/${sample}_R1_trim.fastq.gz \
+            /home/weber/fastp_trim/${sample}/${sample}_R2_trim.fastq.gz \
+    --read-group-id-prefix ${sample} --read-group-sm ${sample} --read-group-lb lib1 --read-group-pl ILLUMINA \
+    --out-bam /home/weber/outputdir/${sample}/${sample}_fq2bam_output.bam \
+    --out-duplicate-metrics /home/weber/outputdir/${sample}/${sample}.dup_metrics.txt \
     --tmp-dir /home/weber/tmp/ \
     --low-memory
 
@@ -109,11 +122,11 @@ docker run \
   --volume /home:/home \
   nvcr.io/nvidia/clara/clara-parabricks:4.6.0-1 \
   pbrun bqsr  \
-    --ref /home/weber/ref/Homo_sapiens_assembly38.fasta \
-    --in-bam /home/weber/outputdir/fq2bam_output.bam \
+    --ref /home/weber/ref/${ref} \
+    --in-bam /home/weber/outputdir/${sample}/${sample}_fq2bam_output.bam \
     --knownSites /home/weber/known_sites/dbsnp_146.hg38.vcf.gz \
     --knownSites /home/weber/known_sites/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz \
-    --out-recal-file /home/weber/outputdir/test_sample.recal_data.table
+    --out-recal-file /home/weber/outputdir/${sample}/${sample}.recal_data.table
 
 
 # Apply BQSR
@@ -123,10 +136,10 @@ docker run \
   -v /home:/home \
   nvcr.io/nvidia/clara/clara-parabricks:4.6.0-1 \
   pbrun applybqsr \
-  --ref /home/weber/ref/Homo_sapiens_assembly38.fasta \
-  --in-bam /home/weber/outputdir/fq2bam_output.bam \
-  --in-recal-file /home/weber/outputdir/test_sample.recal_data.table \
-  --out-bam /home/weber/outputdir/test_sample.recal.bam
+  --ref /home/weber/ref/${ref} \
+  --in-bam /home/weber/outputdir/${sample}/${sample}_fq2bam_output.bam \
+  --in-recal-file /home/weber/outputdir/${sample}/${sample}.recal_data.table \
+  --out-bam /home/weber/outputdir/${sample}/${sample}.recal.bam
 
 # HaplotypeCaller
 docker run \
@@ -135,10 +148,10 @@ docker run \
   -v /home:/home \
   nvcr.io/nvidia/clara/clara-parabricks:4.6.0-1 \
   pbrun haplotypecaller \
-  --ref /home/weber/ref/Homo_sapiens_assembly38.fasta \
-  --in-bam /home/weber/outputdir/test_sample.recal.bam \
-  --logfile /home/weber/outputdir/logs/test_sample.Haplotypecaller.log \
-  --out-variants /home/weber/outputdir/test_sample_2.vcf.gz
+  --ref /home/weber/ref/${ref} \
+  --in-bam /home/weber/outputdir/${sample}/${sample}.recal.bam \
+  --logfile /home/weber/outputdir/logs/${sample}.Haplotypecaller.log \
+  --out-variants /home/weber/outputdir/${sample}/${sample}.haplotypecaller.vcf.gz
 
 
  # gvcf /gvcf不能filter
@@ -148,11 +161,11 @@ docker run \
   -v /home:/home \
   nvcr.io/nvidia/clara/clara-parabricks:4.6.0-1 \
   pbrun haplotypecaller \
-  --ref /home/weber/ref/Homo_sapiens_assembly38.fasta \
-  --in-bam /home/weber/outputdir/test_sample.recal.bam \
-  --logfile /home/weber/outputdir/logs/test_sample.Haplotypecaller.g.log \
+  --ref /home/weber/ref/${ref} \
+  --in-bam /home/weber/outputdir/${sample}/${sample}.recal.bam \
+  --logfile /home/weber/outputdir/logs/${sample}.Haplotypecaller.g.log \
   --gvcf \
-  --out-variants /home/weber/outputdir/test_sample_2.g.vcf.gz
+  --out-variants /home/weber/outputdir/${sample}/${sample}.haplotypecaller.g.vcf.gz
 
 
 # Deepvariant
@@ -162,11 +175,11 @@ docker run \
   -v /home:/home \
   nvcr.io/nvidia/clara/clara-parabricks:4.6.0-1 \
   pbrun deepvariant \
-  --ref /home/weber/ref/Homo_sapiens_assembly38.fasta \
-  --in-bam /home/weber/outputdir/test_sample.recal.bam \
-  --out-variants /home/weber/outputdir/test_sample.deepvariant_2.vcf.gz \
+  --ref /home/weber/ref/${ref} \
+  --in-bam /home/weber/outputdir/${sample}/${sample}.recal.bam \
+  --out-variants /home/weber/outputdir/${sample}/${sample}.deepvariant.vcf.gz \
   --num-gpus 8 \
-  --logfile /home/weber/outputdir/logs/test_sample.deepvariant.log
+  --logfile /home/weber/outputdir/logs/${sample}.deepvariant.log
 
 
 docker run \
@@ -175,20 +188,20 @@ docker run \
   -v /home:/home \
   nvcr.io/nvidia/clara/clara-parabricks:4.6.0-1 \
   pbrun deepvariant \
-  --ref /home/weber/ref/Homo_sapiens_assembly38.fasta \
-  --in-bam /home/weber/outputdir/test_sample.recal.bam \
-  --out-variants /home/weber/outputdir/test_sample.deepvariant_2.g.vcf.gz \
+  --ref /home/weber/ref/${ref} \
+  --in-bam /home/weber/outputdir/${sample}/${sample}.recal.bam \
+  --out-variants /home/weber/outputdir/${sample}/${sample}.deepvariant.g.vcf.gz \
   --num-gpus 8 \
   --gvcf \
-  --logfile /home/weber/outputdir/logs/test_sample.deepvariant.g.log
+  --logfile /home/weber/outputdir/logs/${sample}.deepvariant.g.log
 
 
 # filter(vcf)
 docker run --rm -v /home/weber:/data broadinstitute/gatk:4.3.0.0 \
   gatk VariantFiltration \
-    -R /data/ref/Homo_sapiens_assembly38.fasta \
-    -V /data/outputdir/test_sample_2.vcf.gz \
-    -O /data/outputdir/test_sample_filtered_2.vcf.gz \
+    -R /data/ref/${ref} \
+    -V /data/outputdir/${sample}/${sample}.haplotypecaller.vcf.gz \
+    -O /data/outputdir/${sample}/${sample}_filtered.haplotypecaller.vcf.gz \
     --filter-name "HardQUAL" --filter-expression "QUAL < 3.0103" \
     --filter-name "LowGQ" --filter-expression "GQ < 0" \
     --filter-name "LowDepth" --filter-expression "DP < 1" \
@@ -196,88 +209,12 @@ docker run --rm -v /home/weber:/data broadinstitute/gatk:4.3.0.0 \
 
 docker run --rm -v /home/weber:/data broadinstitute/gatk:4.3.0.0 \
   gatk VariantFiltration \
-    -R /data/ref/Homo_sapiens_assembly38.fasta \
-    -V /data/outputdir/test_sample.deepvariant_2.vcf.gz \
-    -O /data/outputdir/test_sample_filtered.deepvariant_2.vcf.gz \
+    -R /data/ref/${ref} \
+    -V /data/outputdir/${sample}/${sample}.deepvariant.vcf.gz \
+    -O /data/outputdir/${sample}/${sample}_filtered.deepvariant.vcf.gz \
     --filter-name "HardQUAL" --filter-expression "QUAL < 3.0103" \
     --filter-name "LowGQ" --filter-expression "GQ < 0" \
     --filter-name "LowDepth" --filter-expression "DP < 1" \
     --filter-name "LowAF" --filter-expression "AF < 0.2"
-
-"""
-#查看 dragen 格式
-bcftools index -o $HOME/00015988_2025-09-12.hard-filtered.gvcf.gz.tbi /mnt/NovaSeqX/GalateaBio/plate1/gvcf/00015988_2025-09-12_ds.21417714ac12427194e62287ee5d0953/00015988_2025-09-12.hard-filtered.gvcf.gz
-
-ln -s /mnt/NovaSeqX/GalateaBio/plate1/gvcf/00015988_2025-09-12_ds.21417714ac12427194e62287ee5d0953/00015988_2025-09-12.hard-filtered.gvcf.gz $HOME/
-zcat 00015988_2025-09-12.hard-filtered.gvcf.gz | less -S
-
-# 未trim版統計
-docker run --rm -u $(id -u):$(id -g) -v /home/weber:/data biocontainers/bcftools:v1.9-1-deb_cv1 \
-bcftools query -f '%FILTER\n' /data/outputdir/test_sample_filtered.vcf.gz | sort | uniq -c
-
-docker run --rm -u $(id -u):$(id -g) -v /home/weber:/data biocontainers/bcftools:v1.9-1-deb_cv1 \
-bcftools view -H /data/outputdir/test_sample.g.vcf.gz | wc -l
-
-docker run --rm -u $(id -u):$(id -g) -v /home/weber:/data biocontainers/bcftools:v1.9-1-deb_cv1 \
-bcftools query -f '%FILTER\n' /data/outputdir/test_sample_filtered.deepvariant.vcf.gz | sort | uniq -c
-
-docker run --rm -u $(id -u):$(id -g) -v /home/weber:/data biocontainers/bcftools:v1.9-1-deb_cv1 \
-bcftools view -H /data/outputdir/test_sample.deepvariant.g.vcf.gz | wc -l
-
-docker run --rm -u 1014:1017 -v /mnt/NovaSeqX/GalateaBio/plate1:/data biocontainers/bcftools:v1.9-1-deb_cv1 \
-bcftools query -f '%FILTER\n' /data/gvcf/00015988_2025-09-12_ds.21417714ac12427194e62287ee5d0953/00015988_2025-09-12.hard-filtered.gvcf.gz | sort | uniq -c
-
-
-bcftools query -f '%FILTER\n' /mnt/NovaSeqX/GalateaBio/plate1/gvcf/00015988_2025-09-12_ds.21417714ac12427194e62287ee5d0953/00015988_2025-09-12.hard-filtered.gvcf.gz | \
-awk '{filter=($0=="PASS")?"PASS":"non-PASS"; count[filter]++} END{for(f in count) print f, count[f]}'
-
-for CHR in {1..22} X Y M; do
-  echo " chr$CHR :"
-  docker run --rm -u $(id -u):$(id -g) -v /home/weber:/data biocontainers/bcftools:v1.9-1-deb_cv1 \
-  bcftools query -r chr$CHR -f '%FILTER\n' /data/outputdir/test_sample_filtered.vcf.gz | sort | uniq -c
-done
-
-for CHR in {1..22} X Y M; do
-  echo  " chr$CHR :"
-  docker run --rm -u $(id -u):$(id -g) -v /home/weber:/data biocontainers/bcftools:v1.9-1-deb_cv1 \
-  bcftools view -H -r chr$CHR /data/outputdir/test_sample.g.vcf.gz | wc -l
-done
-
-for CHR in {1..22} X Y M; do
-  echo " chr$CHR :"
-  docker run --rm -u $(id -u):$(id -g) -v /home/weber:/data biocontainers/bcftools:v1.9-1-deb_cv1 \
-  bcftools query -r chr$CHR -f '%FILTER\n' /data/outputdir/test_sample_filtered.deepvariant.vcf.gz | sort | uniq -c
-done
-
-for CHR in {1..22} X Y M; do
-  echo  " chr$CHR :"
-  docker run --rm -u $(id -u):$(id -g) -v /home/weber:/data biocontainers/bcftools:v1.9-1-deb_cv1 \
-  bcftools view -H -r chr$CHR /data/outputdir/test_sample.deepvariant.g.vcf.gz | wc -l
-done
-
-for CHR in {1..22} X Y M; do
-  echo " chr$CHR:"
-  bcftools query -r chr$CHR -f '%FILTER\n' $HOME/00015988_2025-09-12.hard-filtered.gvcf.gz | \
-  awk '{filter=($0=="PASS")?"PASS":"non-PASS"; count[filter]++} END{for(f in count) print f, count[f]}'
-done
-"""
-
-
-
-# trim版 統計
-docker run --rm -u $(id -u):$(id -g) -v /home/weber:/data biocontainers/bcftools:v1.9-1-deb_cv1 \
-bcftools query -f '%FILTER\n' /data/outputdir/test_sample_filtered_2.vcf.gz | sort | uniq -c
-
-docker run --rm -u $(id -u):$(id -g) -v /home/weber:/data biocontainers/bcftools:v1.9-1-deb_cv1 \
-bcftools view -H /data/outputdir/test_sample_2.g.vcf.gz | wc -l
-
-docker run --rm -u $(id -u):$(id -g) -v /home/weber:/data biocontainers/bcftools:v1.9-1-deb_cv1 \
-bcftools query -f '%FILTER\n' /data/outputdir/test_sample_filtered.deepvariant_2.vcf.gz | sort | uniq -c
-
-docker run --rm -u $(id -u):$(id -g) -v /home/weber:/data biocontainers/bcftools:v1.9-1-deb_cv1 \
-bcftools view -H /data/outputdir/test_sample.deepvariant_2.g.vcf.gz | wc -l
-
-
-
 
 
