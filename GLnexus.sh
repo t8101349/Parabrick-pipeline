e1d4edaa01addb8849264c9ce76342e3a37cce7c
t8101349 <30904854+t8101349@users.noreply.github.com>
2025-12-01 15:01:16 +0800

    Add files via upload
    add GLnexus

---
+++ GLnexus.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# gvcf 整合於一個資料夾
+# dpv/sample1.g.vcf.gz
+# dpv/sample2.g.vcf.gz
+
+# 製作清單
+ls dpv/*.g.vcf.gz > gvcf.list
+
+# glnexus_cli
+glnexus_cli --config DeepVariantWGS --list gvcf.list \
+  | bgzip -c > merge_corhort.vcf.gz
+tabix -p vcf merge_corhort.vcf.gz
